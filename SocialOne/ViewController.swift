6734a5cc812fbdfcb39eec42d948bd9562b14a30
benny0912 <bennykeanhoe.ooi@sjsu.edu>
2020-04-18 21:39:53 -0700

    Initial Commit

---
+++ SocialOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialOne
+//
+//  Created by Benny Ooi Kean Hoe on 4/18/20.
+//  Copyright © 2020 Benny Ooi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
