1c76d9905202180f0d5791795c8d1b71d8444965
admin <admin@USCS-Mac243.local>
2020-05-05 09:54:22 -0700

    Facebook Share button works

--- SocialOne/Controller/PostViewController.swift
+++ SocialOne/Controller/PostViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import AlamofireImage
+import FBSDKShareKit
 
 class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var postImage: UIImageView!
     
     @IBOutlet weak var commentTextField: UITextField!
+    
+    let content = SharePhotoContent()
     
     
     override func viewDidLoad() {
@@ -26,6 +29,19 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 
     @IBAction func onSubmitButton(_ sender: Any)
     {
+        let button = FBShareButton()
+        button.shareContent = content
+        view.addSubview(button)
+        
+    /*
+
+        let dialog = ShareDialog()
+        dialog.fromViewController = self
+        dialog.shareContent = content
+        dialog.mode = SharePhotoContent
+        dialog.show()*/
+        
+        
         
     }
     
@@ -53,13 +69,33 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
     {
+        
+        //Gathering user Uploaded image
         let image = info [.editedImage] as! UIImage
         let size = CGSize(width: 300, height: 300)
         let scaledImage = image.af_imageScaled(to: size)
         
+        
+        
+        //preparing for the image to be shared with facebook
+        let facebookPhoto = SharePhoto()
+        facebookPhoto.image = scaledImage
+        facebookPhoto.isUserGenerated = true
+        if let facebookCaption = commentTextField.text //if there is a caption
+        {
+            print("Facebook Caption: \(facebookCaption)")
+            facebookPhoto.caption = facebookCaption
+        }
+       
+        content.photos = [facebookPhoto]
+        
+        //Showing the image that was selected on screen.
         postImage.image =  scaledImage
         
         dismiss(animated: true, completion: nil)
+        
+
+
         
         
         
