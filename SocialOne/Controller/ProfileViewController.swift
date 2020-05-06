650c33e1bcbdfa1e5ffa85b0d0045dfa735c0260
admin <admin@USCS-Mac243.local>
2020-05-06 15:22:58 -0700

    eliminated extra code

--- SocialOne/Controller/ProfileViewController.swift
+++ SocialOne/Controller/ProfileViewController.swift
@@ -12,7 +12,7 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 import SwiftyJSON
 
-class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ProfileViewController: UIViewController{
 
     @IBOutlet weak var facebookProfileImage: UIImageView!
     @IBOutlet weak var facebookFriendsCountLabel: UILabel!
@@ -39,109 +39,6 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         delegate.window?.rootViewController = loginViewController
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
- 
-        if indexPath.row == 0 {
-            //Instagram Cell
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InstagramCollectionCell", for: indexPath) as! InstagramCollectionViewCell
-            
-            //Not log in
-            cell.numPostLabel.text = ""
-            cell.numFollowerLabel.text = ""
-            cell.numFollowingLabel.text = ""
-            cell.postLabel.isHidden = true
-            cell.followingLabel.isHidden = true
-            cell.followersLabel.isHidden = true
-            cell.profileImageView.isHidden = true
-            cell.loginInstagramButton.isHidden = false
-            cell.signOutInstagramButton.isEnabled = false
-            cell.nameLabel.text = ""
-            
-            
-            cell.contentView.layer.cornerRadius = 4.0
-            cell.contentView.layer.borderWidth = 1.0
-            cell.contentView.layer.borderColor = UIColor.clear.cgColor
-            cell.contentView.layer.masksToBounds = false
-            cell.layer.shadowColor = UIColor.gray.cgColor
-            cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
-            cell.layer.shadowRadius = 4.0
-            cell.layer.shadowOpacity = 1.0
-            cell.layer.masksToBounds = false
-            cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
-            return cell
-        } else if indexPath.row == 1 {
-            //Facebook Cell
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FacebookCollectionCell", for: indexPath) as! FacebookCollectionViewCell
-            
-            //Not log in
-            cell.numFriendLabel.text = ""
-            cell.nameLabel.text = ""
-            cell.friendsLabel.isHidden = true
-            cell.loginButton.isHidden = false
-            cell.numPhotosLabel.text = ""
-            cell.photosLabel.isHidden = true
-            cell.signOutButton.isEnabled = false
-            cell.userImageView.isHidden = true
-            
-            cell.contentView.layer.cornerRadius = 4.0
-            cell.contentView.layer.borderWidth = 1.0
-            cell.contentView.layer.borderColor = UIColor.clear.cgColor
-            cell.contentView.layer.masksToBounds = false
-            cell.layer.shadowColor = UIColor.gray.cgColor
-            cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
-            cell.layer.shadowRadius = 4.0
-            cell.layer.shadowOpacity = 1.0
-            cell.layer.masksToBounds = false
-            cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
-            return cell
-        } else {
-            //Twitter Cell
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TwitterCollectionCell", for: indexPath) as! TwitterCollectionViewCell
-            if UserDefaults.standard.bool(forKey: "twitterUserLoggedIn") {
-                cell.followersLabel.isHidden = false
-                cell.followingLabel.isHidden = false
-                cell.loginTwitterButton.isHidden = true
-                cell.profileImageView.isHidden = false
-                cell.signoutTwitterButton.isEnabled = true
-                cell.tweetsLabel.isHidden = false
-                
-               
-                
-                cell.nameLabel.text = ""
-                cell.numFollowersLabel.text = ""
-                cell.numFollowingLabel.text = ""
-                cell.numTweetsLabel.text = ""
-            } else {
-                //Not logged in
-                cell.followersLabel.isHidden = true
-                cell.followingLabel.isHidden = true
-                cell.loginTwitterButton.isHidden = false
-                cell.nameLabel.text = ""
-                cell.numFollowersLabel.text = ""
-                cell.numFollowingLabel.text = ""
-                cell.numTweetsLabel.text = ""
-                cell.profileImageView.isHidden = true
-                cell.signoutTwitterButton.isEnabled = false
-                cell.tweetsLabel.isHidden = true
-            }
-            
-            cell.contentView.layer.cornerRadius = 4.0
-            cell.contentView.layer.borderWidth = 1.0
-            cell.contentView.layer.borderColor = UIColor.clear.cgColor
-            cell.contentView.layer.masksToBounds = false
-            cell.layer.shadowColor = UIColor.gray.cgColor
-            cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
-            cell.layer.shadowRadius = 4.0
-            cell.layer.shadowOpacity = 1.0
-            cell.layer.masksToBounds = false
-            cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
-            return cell
-        }
-    }
     
     @IBAction func onFacebookLoginButton(_ sender: Any) {
         
